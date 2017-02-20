4ad200b819f96671ede56ab292f34f699557d04e
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2017-02-20 14:49:15 -0800

    Make each client append to a unique color.

--- examples/or-set/src/worker.cc
+++ examples/or-set/src/worker.cc
@@ -6,7 +6,7 @@ worker::worker(config cfg)
 	_color = (struct colors*)malloc(sizeof(struct colors));
         _color->numcolors = 1;
         _color->mycolors = new ColorID[0];
-        _color->mycolors[0] = 0; 
+        _color->mycolors[0] = cfg.server_id; 
 	_log_client = new_dag_handle_for_single_server(_log_addr.c_str(), _color);
 	_proc_id = cfg.server_id;
 	_or_set = new or_set(_log_client, _color, cfg.server_id, cfg.sync_duration);
