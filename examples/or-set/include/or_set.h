8d7538101de4f90c02049142d5ef0fd8e43b3823
Joshua Lockerman <>
2018-04-30 17:17:52 -0400

    Or-set transactions

--- examples/or-set/include/or_set.h
+++ examples/or-set/include/or_set.h
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <set>
 #include <cstdint>
@@ -18,45 +18,47 @@ public:
 		ADD = 0,
 		REMOVE = 1,
 		READ = 2,
+		TRANSACTION = 3,
 	} log_opcode;
-		
+
 
 private:
-	
+
 	struct colors 						*_color;
 	struct colors 						*_remote_colors;
 	DAGHandle 						*_log_client;
 	std::unordered_map<uint64_t, std::set<uint64_t> > 	_state;
  	uint64_t 						_guid_counter;
-	uint8_t							_proc_id;	
+	uint8_t							_proc_id;
 	char 							_buf[DELOS_MAX_DATA_SIZE];
-	std::chrono::microseconds				_sync_duration; 
-	std::mutex						_instance_mutex; 
+	std::chrono::microseconds				_sync_duration;
+	std::mutex						_instance_mutex;
 	std::thread 						_sync_thread;
 
 	/* Opcode logic for fuzzy log entries */
-	uint64_t make_opcode(log_opcode opcode);	
+	uint64_t make_opcode(log_opcode opcode);
 	log_opcode get_opcode(uint64_t code);
-	uint8_t get_proc_id(uint64_t code);	
-	
+	uint8_t get_proc_id(uint64_t code);
+
 	/* Serialization logic for communication via the fuzzy log. */
 	void serialize_add(uint64_t e, uint64_t guid, char **buf, size_t *sz);
 	void serialize_remove(uint64_t e, const std::set<uint64_t> &guid_set, char **buf, size_t *sz);
 	void deserialize_add(const uint8_t *buf, size_t sz, uint64_t *e, uint64_t *guid);
 	void deserialize_remove(const uint8_t *buf, size_t sz, uint64_t *e, std::set<uint64_t> *guids);
-	
+
 	/* Guid generator for add operations. */
 	uint64_t gen_guid();
-	
+
 	/* Send local add/remove info to the fuzzy log. */
 	void send_add(uint64_t e, uint64_t guid);
 	void send_remove(uint64_t e, const std::set<uint64_t> &guid_set);
 	write_id send_add_async(uint64_t e, uint64_t guid, char *buf);
 	write_id send_remove_async(uint64_t e, const std::set<uint64_t> &guid_set, char *buf);
 
-	/* Receive and process remote add/remove info from the fuzzy log. */	
+	/* Receive and process remote add/remove info from the fuzzy log. */
 	void remote_remove(const uint8_t *buf, size_t sz);
 	void remote_add(const uint8_t *buf, size_t sz);
+	void remote_transaction(const uint8_t *buf, size_t sz);
 
 	/* Add and remove elements from local copy of the or-set */
 	void do_add(uint64_t e, uint64_t guid);
@@ -70,29 +72,30 @@ public:
 
 
 
-	/* 
-	 * handle is created by the caller. 
-	 * 
+	/*
+	 * handle is created by the caller.
+	 *
 	 * color is the set colors the local node is interested in.
 	 *
-	 * proc_id is a unique identifier associated with each or-set instance. 
+	 * proc_id is a unique identifier associated with each or-set instance.
 	 *
 	 * sync_duration is the interval between consecutive calls to synchronize with other
-	 * instances' updates. 
+	 * instances' updates.
 	 *
-	 */ 
+	 */
   	or_set(DAGHandle *handle, struct colors *local_color, struct colors *remote_colors, uint8_t proc_id, uint64_t sync_duration);
-	
+
 	/* Returns true if e exists in the local set */
 	bool lookup(uint64_t e);
 
 	/* Add e to the or-set */
 	void add(uint64_t e);
-	
+
 	/* Remove e from the or-set */
 	void remove(uint64_t e);
-	
+
 	bool get_single_remote();
 	write_id async_add(uint64_t e, char *buf);
 	write_id async_remove(uint64_t e, char *buf);
+	write_id async_transaction(uint64_t* es, bool *remove, size_t elems);
 };
--- examples/or-set/src/or_set.cc
+++ examples/or-set/src/or_set.cc
@@ -1,8 +1,9 @@
 #include <or_set.h>
 #include <cassert>
 #include <iostream>
+#include <vector>
 
-or_set::or_set(DAGHandle *handle, struct colors *color, struct colors *remote_colors, uint8_t proc_id, 
+or_set::or_set(DAGHandle *handle, struct colors *color, struct colors *remote_colors, uint8_t proc_id,
 	       uint64_t sync_duration)
 {
 	_log_client = handle;
@@ -12,10 +13,10 @@ or_set::or_set(DAGHandle *handle, struct colors *color, struct colors *remote_co
 	_guid_counter = 0;
 	_state.clear();
 	_sync_duration = std::chrono::microseconds(sync_duration);
-	
+
 	snapshot(_log_client);
 	/* Turn off anti-entropy for the time being */
-	//	_sync_thread = 	std::thread(&or_set::check_remotes, this); 
+	//	_sync_thread = 	std::thread(&or_set::check_remotes, this);
 }
 
 uint64_t or_set::make_opcode(log_opcode opcode)
@@ -38,47 +39,50 @@ bool or_set::get_single_remote()
 	size_t buf_sz;
 	struct colors c;
 	uint64_t *uint_buf;
-	
+
 	get_next(_log_client, _buf, &buf_sz, &c);
-	
+
 	if (c.numcolors == 0) {
-		snapshot(_log_client);	
+		snapshot(_log_client);
 		return false;
 	}
-		
+
 	uint_buf = reinterpret_cast<uint64_t*>(_buf);
-	auto opcode = uint_buf[0];			
+	auto opcode = uint_buf[0];
 
 	switch (get_opcode(opcode)) {
-	case ADD: 
+	case ADD:
 		remote_add((uint8_t*)_buf, buf_sz);
 		break;
-	case REMOVE:	
+	case REMOVE:
 		remote_remove((uint8_t*)_buf, buf_sz);
+		break;
+	case TRANSACTION:
+		remote_transaction((uint8_t*)_buf, buf_sz);
 		break;
 	default:
 		/* XXX Debugging log playback issue. */
 		std::cerr << std::showbase << std::hex;
-		std::cerr << opcode << "\n";	
+		std::cerr << opcode << "\n";
 		assert(false);
 	}
 	return true;
 
 	/*
-	size_t data_sz, locs_read; 
-	auto entry = get_next2(_log_client, &data_sz, &locs_read);		
-	
+	size_t data_sz, locs_read;
+	auto entry = get_next2(_log_client, &data_sz, &locs_read);
+
 	if (locs_read == 0) {
 		snapshot_colors(_log_client, _remote_colors);
 		return false;
-	}	
-	
-	auto uint_buf = reinterpret_cast<const uint64_t*>(entry.data);	
-	auto opcode = uint_buf[0];	
+	}
+
+	auto uint_buf = reinterpret_cast<const uint64_t*>(entry.data);
+	auto opcode = uint_buf[0];
 	assert(get_proc_id(opcode) != _proc_id);
 	switch (get_opcode(opcode)) {
 	case ADD:
-		remote_add(entry.data, data_sz); 
+		remote_add(entry.data, data_sz);
 		break;
 	case REMOVE:
 		remote_remove(entry.data, data_sz);
@@ -86,7 +90,7 @@ bool or_set::get_single_remote()
 	default:
 		std::cerr << std::showbase << std::hex;
 		std::cerr << opcode << "\n";
-		assert(false); 
+		assert(false);
 	}
 	assert(false);
 	return true;
@@ -99,7 +103,7 @@ void or_set::get_remote_updates()
 	size_t buf_sz;
 	struct colors c;
 	uint64_t *uint_buf;
-	
+
 	/* XXX Shouldn't get here! */
 	assert(false);
 }
@@ -115,7 +119,7 @@ void or_set::check_remotes()
 	}
 }
 
-uint64_t or_set::gen_guid() 
+uint64_t or_set::gen_guid()
 {
 	auto guid = _guid_counter;
 	_guid_counter += 1;
@@ -136,17 +140,17 @@ void or_set::serialize_add(uint64_t e, uint64_t guid, char **buf, size_t *sz)
 	rbuf[0] = make_opcode(ADD);
 	rbuf[1] = e;
 	rbuf[2] = guid;
-	
+
 	*sz = sizeof(uint64_t)*3;
-	return;		
+	return;
 }
 
 void or_set::send_add(uint64_t e, uint64_t guid)
 {
 	char *buf;
 	size_t sz;
-	
-	buf = NULL;	
+
+	buf = NULL;
 	serialize_add(e, guid, &buf, &sz);
 	append(_log_client, buf, sz, _color, NULL);
 }
@@ -154,7 +158,7 @@ void or_set::send_add(uint64_t e, uint64_t guid)
 write_id or_set::send_add_async(uint64_t e, uint64_t guid, char *buf)
 {
 	size_t sz;
-	serialize_add(e, guid, &buf, &sz);	
+	serialize_add(e, guid, &buf, &sz);
 //	std::cerr << "Serialized request!\n";
 //	std::cerr << "Request size: " << sz << "\n";
 	return async_append(_log_client, buf, sz, _color, NULL);
@@ -164,15 +168,15 @@ write_id or_set::send_add_async(uint64_t e, uint64_t guid, char *buf)
 void or_set::serialize_remove(uint64_t e, const std::set<uint64_t> &guid_set, char **buf, size_t *sz)
 {
 	assert(guid_set.size() > 0);
-	
-	uint64_t *rbuf;	
+
+	uint64_t *rbuf;
 
 	if (*buf == NULL) {
 		rbuf = static_cast<uint64_t*>(malloc(sizeof(uint64_t)*2 + sizeof(uint64_t)*guid_set.size()));
 		*buf = reinterpret_cast<char*>(rbuf);
 	} else {
-		rbuf = reinterpret_cast<uint64_t*>(*buf);		
-	}	
+		rbuf = reinterpret_cast<uint64_t*>(*buf);
+	}
 	rbuf[0] = make_opcode(REMOVE);
 
 	rbuf[1] = e;
@@ -181,7 +185,7 @@ void or_set::serialize_remove(uint64_t e, const std::set<uint64_t> &guid_set, ch
 	for (auto guid : guid_set) {
 		rbuf[i] = guid;
 		i += 1;
-	}	
+	}
 
 	*sz = sizeof(uint64_t)*(2 + guid_set.size());
 }
@@ -190,8 +194,8 @@ void or_set::send_remove(uint64_t e, const std::set<uint64_t> &guid_set)
 {
 	char *buf;
 	size_t sz;
-	
-	buf = NULL;	
+
+	buf = NULL;
 	serialize_remove(e, guid_set, &buf, &sz);
 	append(_log_client, buf, sz, _color, NULL);
 }
@@ -200,14 +204,14 @@ write_id or_set::send_remove_async(uint64_t e, const std::set<uint64_t> &guid_se
 {
 	assert(buf != NULL);
 	size_t sz;
-	
+
 	serialize_remove(e, guid_set, &buf, &sz);
 	return async_append(_log_client, buf, sz, _color, NULL);
 }
 
 void or_set::do_add(uint64_t e, uint64_t guid)
 {
-	_state[e].insert(guid);	
+	_state[e].insert(guid);
 }
 
 void or_set::do_remove(uint64_t e, const std::set<uint64_t> &guid_set)
@@ -216,25 +220,25 @@ void or_set::do_remove(uint64_t e, const std::set<uint64_t> &guid_set)
 		for (auto guid : guid_set) {
 			_state[e].erase(guid);
 		}
-		
+
 		if (_state[e].size() == 0)
 			_state.erase(e);
-	}	
+	}
 }
 
 void or_set::remote_remove(const uint8_t *buf, size_t sz)
 {
 	uint64_t e;
 	std::set<uint64_t> guids;
-	
-	deserialize_remove(buf, sz, &e, &guids);	
+
+	deserialize_remove(buf, sz, &e, &guids);
 	do_remove(e, guids);
 }
 
 void or_set::remote_add(const uint8_t *buf, size_t sz)
 {
 	uint64_t e, guid;
-	
+
 	deserialize_add(buf, sz, &e, &guid);
 	do_add(e, guid);
 }
@@ -244,28 +248,62 @@ void or_set::deserialize_remove(const uint8_t *buf, size_t sz, uint64_t *e, std:
 	auto uint_buf = reinterpret_cast<const uint64_t*>(buf);
 	auto opcode = get_opcode(uint_buf[0]);
 	assert(opcode == REMOVE);
-	
+
 	*e = uint_buf[1];
-	
+
 	auto num_guids = sz / sizeof(uint64_t);
-	for (auto i = 2; i < num_guids; ++i) 
-		guids->insert(uint_buf[i]);				
+	for (auto i = 2; i < num_guids; ++i)
+		guids->insert(uint_buf[i]);
 }
 
-void or_set::deserialize_add(const uint8_t *buf, size_t sz, uint64_t *e, uint64_t *guid) 
+void or_set::deserialize_add(const uint8_t *buf, size_t sz, uint64_t *e, uint64_t *guid)
 {
 	auto uint_buf = reinterpret_cast<const uint64_t*>(buf);
-	auto opcode = get_opcode(uint_buf[0]); 
+	auto opcode = get_opcode(uint_buf[0]);
 	assert(opcode == ADD && sz == sizeof(uint64_t)*3);
 
 	*e = uint_buf[1];
 	*guid = uint_buf[2];
 }
 
+void or_set::remote_transaction(const uint8_t *buf, size_t sz)
+{
+	auto uint_buf = reinterpret_cast<const uint64_t*>(buf);
+	auto opcode = get_opcode(uint_buf[0]);
+	assert(opcode == TRANSACTION);
+	auto elems = uint_buf[1];
+	for(size_t i = 2; i < elems;) {
+		switch (uint_buf[i]) {
+		case ADD: {
+			auto to_add = uint_buf[i++];
+			auto guid = uint_buf[i++];
+			do_add(to_add, guid);
+			break;
+		}
+		break;
+		case REMOVE: {
+			std::set<uint64_t> guid_set;
+			auto to_remove = uint_buf[i++];
+			auto size = uint_buf[i++];
+			for (int j = 0; j < size; j++) guid_set.insert(uint_buf[i++]);
+			do_remove(to_remove, guid_set);
+			break;
+		}
+		break;
+		default: {
+			/* XXX Debugging log playback issue. */
+			std::cerr << std::showbase << std::hex;
+			std::cerr << uint_buf[i] << "\n";
+			assert(false);
+			return;
+		}}
+	}
+}
+
 bool or_set::lookup(uint64_t e)
 {
 	{
-		std::lock_guard<std::mutex> lck(_instance_mutex);		
+		std::lock_guard<std::mutex> lck(_instance_mutex);
 		return _state.count(e) > 0;
 	}
 }
@@ -276,7 +314,7 @@ void or_set::add(uint64_t e)
 		std::lock_guard<std::mutex> lck(_instance_mutex);
 		auto guid = gen_guid();
 		do_add(e, guid);
-		send_add(e, guid);	
+		send_add(e, guid);
 	}
 }
 
@@ -295,9 +333,9 @@ write_id or_set::async_add(uint64_t e, char *buf)
 	{
 		std::lock_guard<std::mutex> lck(_instance_mutex);
 		auto guid = gen_guid();
-		do_add(e, guid);	
+		do_add(e, guid);
 		return send_add_async(e, guid, buf);
-	}	
+	}
 }
 
 write_id or_set::async_remove(uint64_t e, char *buf)
@@ -305,10 +343,52 @@ write_id or_set::async_remove(uint64_t e, char *buf)
 	{
 		std::lock_guard<std::mutex> lck(_instance_mutex);
 		if (_state.count(e) == 0)
-			return WRITE_ID_NIL;	
+			return WRITE_ID_NIL;
 
 		auto guid_set = _state[e];
 		do_remove(e, guid_set);
 		return send_remove_async(e, guid_set, buf);
+	}
+}
+
+write_id or_set::async_transaction(uint64_t* es, bool *remove, size_t elems) {
+
+	std::vector<uint64_t> buf;
+	buf.push_back(make_opcode(TRANSACTION));
+	buf.push_back((uint64_t)elems);
+
+	{
+		std::lock_guard<std::mutex> lck(_instance_mutex);
+		for(size_t i = 0; i < elems; i++) {
+			auto e = es[i];
+			if (*remove) {
+				if (_state.count(e) == 0) continue;
+				auto guid_set = _state[e];
+				do_remove(e, guid_set);
+
+				buf.push_back(make_opcode(REMOVE));
+				buf.push_back(e);
+				buf.push_back(guid_set.size());
+				for (auto guid : guid_set) {
+					buf.push_back(guid);
+				}
+			} else {
+				auto guid = gen_guid();
+				do_add(e, guid);
+
+				buf.push_back(make_opcode(ADD));
+				buf.push_back(e);
+				buf.push_back(guid);
+			}
+		}
+		if (buf.size() == 1) return WRITE_ID_NIL;
+
+		return async_append(
+			_log_client,
+			reinterpret_cast<char*>(buf.data()),
+			buf.size() * sizeof(uint64_t),
+			_color, //FIXME
+			NULL
+		);
 	}
 }
